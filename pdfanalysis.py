e0ff56bede5e05d2c61afdc5137ef736d474448b
Vraj1103 <svraj073@gmail.com>
2023-09-24 10:26:39 +0530

    chat ui improved

--- pdfanalysis.py
+++ pdfanalysis.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pickle
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -17,8 +18,10 @@ def main():
     st.header("PDF Analysis")
     pdf = st.file_uploader("Upload a PDF", type='pdf')
     st.write(pdf.name)
+    st.write(pdf.type)
 
     if pdf is not None:
+
         pdf_reader = PdfReader(pdf)
         text = ""
         for page in pdf_reader.pages:
@@ -31,8 +34,12 @@ def main():
         )
         chunks = text_splitter.split_text(text=text)
 
-        # embeddings = OpenAIEmbeddings()
-        # vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+        embeddings = OpenAIEmbeddings()
+        vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+        store_name = pdf.name[:-4]
+
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(VectorStore, f)
 
         # st.write(chunks)
         # st.write(text)
